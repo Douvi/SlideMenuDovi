970db9bc319be90cc95a23e4a64ce1b4e483d974
Edouard Roussillon <edouardroussillon@concrete.com.br>
2016-06-05 01:04:18 -0300

    clean code

--- Example/SlideMenuDovi/ListAnimationViewController.swift
+++ Example/SlideMenuDovi/ListAnimationViewController.swift
@@ -9,31 +9,31 @@
 import UIKit
 
 class ListAnimationViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
     private func launchRoot() {
         let root = RootViewController.viewControllerWithStoryboard("Main", controllerName: "RootViewController")
-        
+
         self.navigationController?.presentViewController(root!, animated: true, completion: nil)
     }
-    
+
     //
     // MARK: Click
     //
-    
+
     @IBAction func clickDefault(sender: AnyObject) {
         SliderMenuOptions.animationType = SliderMenuAnimationDefault()
         self.launchRoot()
     }
-    
-    
+
+
     @IBAction func clickSliderOver(sender: AnyObject) {
-        SliderMenuOptions.animationType = SliderMenuAnimationSilderOver()
+        SliderMenuOptions.animationType = SliderMenuAnimationSliderOver()
         self.launchRoot()
     }
-    
-    
+
+
 }
